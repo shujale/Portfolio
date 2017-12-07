03f08617deae97b3ee4c8657cc26553cbbd1b36e
shujale <hujale.sharmarke@gmail.com>
2017-12-07 19:25:28 +0100

    Fixing name

--- assets/js/function.js
+++ assets/js/function.js
@@ -6,7 +6,7 @@ $('#toggle').click(function() {
 
 //Smooth scrolling
 $(function() {
-  $('a.page-scroll').bind('click', function(event) {
+  $('a.scroll').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
       scrollTop: $($anchor.attr('href')).offset().top
